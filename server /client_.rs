85150635d7bea67af50fc88330d25607f3da3410
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-07 03:36:03 +0800

    Update client_.rs

--- server /client_.rs
+++ server /client_.rs
@@ -4,15 +4,12 @@ use std::io::{Read, Write};
 fn main() {
 
     let mut stream = TcpStream::connect("127.0.0.1:7878").expect("Could not connect");
-
     let message = "Hello server";
 
     stream.write_all(message.as_bytes()).expect("Failed to send message");
 
     let mut buffer = [0; 512];
-
     let size = stream.read(&mut buffer).expect("Failed to read response");
-
     let response = String::from_utf8_lossy(&buffer[..size]);
 
     println!("Server says: {}", response);
