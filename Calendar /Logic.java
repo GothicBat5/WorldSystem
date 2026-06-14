72d3753c2eb7b395ed62374424e23439123a31b9
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-14 15:47:02 +0800

    Create Logic.java

---
+++ Calendar /Logic.java
@@ -0,0 +1,46 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class Logic
+{
+    private YearMonth currentMonth;
+
+    public Logic()
+    {
+        currentMonth = YearMonth.now();
+    }
+
+    public void nextMonth()
+    {
+        currentMonth = currentMonth.plusMonths(1);
+    }
+
+    public void previousMonth()
+    {
+        currentMonth = currentMonth.minusMonths(1);
+    }
+
+    public YearMonth getCurrentMonth()
+    {
+        return currentMonth;
+    }
+
+    public int getDaysInMonth()
+    {
+        return currentMonth.lengthOfMonth();
+    }
+
+    public int getStartColumn()
+    {
+        DayOfWeek day =
+                currentMonth.atDay(1).getDayOfWeek();
+
+        return day.getValue() % 7;
+    }
+
+    public LocalDate getToday()
+    {
+        return LocalDate.now();
+    }
+}
