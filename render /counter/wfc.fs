3da0fae6307fceaf8bf270eb11812c3f3bb7ef88
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-24 19:22:44 +0800

    Update wfc.fs

--- render /counter/wfc.fs
+++ render /counter/wfc.fs
@@ -1,20 +1,18 @@
 open System
 
-let readAllInput () =
-    Seq.initInfinite (fun _ -> Console.ReadLine())
+let readAllInput () = Seq.initInfinite (fun _ -> Console.ReadLine())
     |> Seq.takeWhile (fun line -> not (isNull line))
     |> Seq.toList
     |> String.concat " "
 
-let wordFrequency text =
-    text.Split([|' '; '\t'; '\n'; '\r'; '.'; ','; ';'; ':'; '!'|], StringSplitOptions.RemoveEmptyEntries)
+let wordFrequency text = text.Split([|' '; '\t'; '\n'; '\r'; '.'; ','; ';'; ':'; '!'|], 
+StringSplitOptions.RemoveEmptyEntries)
     |> Seq.map (fun w -> w.ToLowerInvariant())
     |> Seq.countBy id
     |> Seq.sortByDescending snd
 
 [<EntryPoint>]
-let main _ =
-    printfn "Paste or type text (Ctrl+Z/Ctrl+D to end):"
+let main _ = printfn "Paste or type text (Ctrl+Z/Ctrl+D to end):"
     let input = readAllInput ()
     let freqs = wordFrequency input
 
